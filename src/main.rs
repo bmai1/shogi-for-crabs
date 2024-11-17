f241abf6a4526a9094734c3aad0e48c4b5a5a0a1
Brian Mai <0hrs3g@gmail.com>
2024-11-16 20:15:02 -0800

    render board image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world");
+use egui::{
+    ViewportBuilder, Context, CentralPanel
+};
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default().with_inner_size([600.0, 600.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Shogi",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx); // image support
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.image(egui::include_image!("images/boards/painting1.jpg"))
+        }); 
+    }
 }
