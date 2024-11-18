ae28b6512871f3cbbfbf4f6a0ddde65d090eb5a0
Brian Mai <0hrs3g@gmail.com>
2024-11-17 18:39:54 -0800

    nightmare nightmare nightmare

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#![allow(unused)]
+
 use egui::{
-    ViewportBuilder, Context, CentralPanel
+    ViewportBuilder, Context, CentralPanel, TextureHandle, Vec2, Image
 };
+
+use std::sync::Arc;
+
+mod image_resources;
+use image_resources::ImageResources;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
@@ -12,18 +19,41 @@ fn main() -> Result<(), eframe::Error> {
         options,
         Box::new(|cc| {
             egui_extras::install_image_loaders(&cc.egui_ctx); // image support
-            Ok(Box::<MyApp>::default())
+            Ok(Box::new(MyApp::new(&cc.egui_ctx))) // init MyApp with context
         }),
     )
 }
 
-#[derive(Default)]
-struct MyApp {}
+// #[derive(Default)]
+struct MyApp {
+    images: Arc<ImageResources>,
+}
+
+impl MyApp {
+    fn new(ctx: &Context) -> Self {
+        let images = Arc::new(ImageResources::new(ctx));
+        Self { images }
+    }
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            ui.image(egui::include_image!("images/boards/painting1.jpg"))
+            // ui.image(&self.images.board);
+            ui.image(egui::include_image!("images/boards/painting1.jpg"));
         }); 
     }
 }
+
+// use shogi::{Move, Position};
+// use shogi::bitboard::Factory as BBFactory;
+// use shogi::square::consts::*;
+
+// fn main() {
+//     BBFactory::init();
+//     let mut pos = Position::new();
+
+//     // initial board position
+//     pos.set_sfen("lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1").unwrap();
+
+// }
